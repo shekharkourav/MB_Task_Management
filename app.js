15fbab79540f8ec74254ed1fc2cb3b615411f7bf
shekharkourav <shekharkourav5424@gmail.com>
2025-05-03 16:03:41 +0530

    Task-Management

--- app.js
+++ app.js
@@ -40,4 +40,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server running on http://localhost:3000'));
+app.listen(PORT, () => console.log('Server running'));
